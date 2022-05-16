21c9d9b33f02277a023220a55b2dcb472413b157
kfdykme <wim.k.f@live.com>
2022-05-17 06:10:20 +0800

    update

--- lib/flutter_desktop_cef_web.dart
+++ lib/flutter_desktop_cef_web.dart
@@ -1,5 +1,7 @@
 library flutter_desktop_cef_web;
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 const kMethodChannelName = "flutter_desktop_cef_web";
@@ -8,11 +10,33 @@ const kMethodChannelName = "flutter_desktop_cef_web";
 class FlutterDesktopCefWeb {
   late MethodChannel mMethodChannel;
 
+  final GlobalKey _containerKey = GlobalKey();
+
   FlutterDesktopCefWeb() {
     mMethodChannel = const MethodChannel(kMethodChannelName);
   }
 
-  loadCef(int x, int y, int width, int height) {
+  Container generateCefContainer(double width, double height) {
+    return Container(
+      key: _containerKey,
+      width: width,
+      height: height,
+      color: Colors.amberAccent,
+    );
+  }
+
+  loadCefContainer() {
+    var size = _containerKey.currentContext!.findRenderObject()!.paintBounds;
+    RenderObject renderObject =
+        _containerKey.currentContext!.findRenderObject()!;
+    RenderBox? box = renderObject as RenderBox?;
+    Offset position = box!.localToGlobal(Offset.zero);
+
+    invokeLoadCef(position.dx.toInt(), position.dy.toInt(), size.width.toInt(),
+        size.height.toInt());
+  }
+
+  invokeLoadCef(int x, int y, int width, int height) {
     print("loadCef ${x} ${y} ${width} ${height}");
     mMethodChannel.invokeMethod("loadCef", <String, Object>{
       'x': x.toString(),
